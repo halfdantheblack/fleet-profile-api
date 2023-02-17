2f7223e13f252f29a7d8265c008d639ed32480b5
halfdantheblack <122974043+halfdantheblack@users.noreply.github.com>
2023-02-17 09:48:41 +0530

    Delete build directory

--- build/config/env-config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configEnvs = void 0;
-const dotenv_1 = require("dotenv");
-const configEnvs = () => {
-    (0, dotenv_1.config)({ path: `.env.${process.env.PLATFORM}` });
-};
-exports.configEnvs = configEnvs;
--- build/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configEnvs = void 0;
-var env_config_1 = require("./env-config");
-Object.defineProperty(exports, "configEnvs", { enumerable: true, get: function () { return env_config_1.configEnvs; } });
