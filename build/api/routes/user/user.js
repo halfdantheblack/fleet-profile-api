410a17fe2ad1d351084e749a521fa8e2f1e96a0a
halfdantheblack <122974043+halfdantheblack@users.noreply.github.com>
2023-02-21 10:35:55 +0530

    Delete build directory

--- build/api/routes/user/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRouter = void 0;
-const express_1 = require("express");
-const controller_1 = require("../../controller");
-exports.userRouter = (0, express_1.Router)();
-exports.userRouter.get('/', controller_1.UserController.getUsers);
-exports.userRouter.post('/', controller_1.UserController.createUser);
--- build/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = exports.envs = void 0;
-var env_config_1 = require("./env-config");
-Object.defineProperty(exports, "envs", { enumerable: true, get: function () { return env_config_1.envs; } });
-var express_1 = require("./express");
-Object.defineProperty(exports, "app", { enumerable: true, get: function () { return express_1.app; } });
